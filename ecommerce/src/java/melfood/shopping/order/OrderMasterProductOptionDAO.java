f35dfca2a79455f6ca059bf786cc04b0448b9c66
Steven Min <steven.jsmin@gmail.com>
2017-07-04 17:01:58 +1000

    Payment Module

--- ecommerce/src/java/melfood/shopping/order/OrderMasterProductOptionDAO.java
+++ ecommerce/src/java/melfood/shopping/order/OrderMasterProductOptionDAO.java
@@ -1,7 +1,55 @@
 package melfood.shopping.order;
 
+import java.util.List;
+
 /**
  * Created by Steven on 4/7/17.
  */
 public interface OrderMasterProductOptionDAO {
+
+    /**
+     * 주문상품의 옵션목록을 가져온다.
+     *
+     * @param orderMasterProductOption
+     * @return
+     * @throws Exception
+     */
+    public List<OrderMasterProductOption> getOrderMasterProductOptions(OrderMasterProductOption orderMasterProductOption) throws Exception;
+
+
+    /**
+     * 주문상품의 옵션(들)을 등록한다.
+     *
+     * @param orderMasterProductOptions
+     * @return
+     * @throws Exception
+     */
+    public Integer insertOrderMasterProductOptions(List<OrderMasterProductOption> orderMasterProductOptions) throws Exception;
+
+    /**
+     * 주문상품의 옵션을 수정한다.
+     *
+     * @param orderMasterProductOption
+     * @return
+     * @throws Exception
+     */
+    public Integer modifyOrderMasterProductOption(OrderMasterProductOption orderMasterProductOption) throws Exception;
+
+    /**
+     * 주문상품의 옵션을 수정한다.
+     *
+     * @param orderMasterProductOption
+     * @return
+     * @throws Exception
+     */
+    public Integer modifyOrderMasterProductOptionForNotNull(OrderMasterProductOption orderMasterProductOption) throws Exception;
+
+    /**
+     * 주문상품의 옵션을 삭제한다.
+     *
+     * @param orderMasterProductOption
+     * @return
+     * @throws Exception
+     */
+    public Integer deleteOrderMasterProductOption(OrderMasterProductOption orderMasterProductOption) throws Exception;
 }
